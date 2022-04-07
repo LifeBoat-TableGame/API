cc237a45d77975bf9cebe5efa5007b5d283eae46
Jeskay <37577958+Jeskay@users.noreply.github.com>
2022-04-07 15:01:28 +1000

    client-server interaction completed

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,14 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+<script lang="ts">
+import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent ({
+  components: {HelloWorld}
+})
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld/>
 </template>
 
 <style>
--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,52 +1,98 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import {io} from 'socket.io-client';
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+export default defineComponent({
+  name: 'HelloWorld',
+  data() {
+    return {
+      title: 'Websockets Tester2',
+      text: '',
+      namet: 'noname',
+      messages: ['Some message', 'Another message'],
+      token: '111',
+      socket: io('http://localhost:3000/chat',  {
+        transportOptions: {
+          polling: {
+            extraHeaders: {
+              Authorization: '12d',
+            }
+          }
+        }
+     }),
+    }
+  },
+  created() {
+    this.initListeners();
+  },
+  methods: {
+      sendMessage() {
+          console.log(`send: ${this.text}`);
+          this.socket.emit('msgToServer', this.text);
+          this.text = "";
+      },
+      renameUser() {
+          console.log(`rename: ${this.namet}`);
+          this.socket.emit('rename', this.namet);
+      },
+      register() {
+        console.log(`register: ${this.namet}`);
+          this.socket.emit('register');
+      },
+      receiveMessage(msg: string) {
+          console.log(`recv: ${msg}`);
+          this.messages.push(msg);
+      },
+      getSupply() {
+          this.socket.emit('getSupply', 'Water');
+      },
+      useToken(token: string) {
+          console.log(`useToken: ${token}`);
+          const socketOptions = {
+              transportOptions: {
+                polling: {
+                  extraHeaders: {
+                    Authorization: token,
+                  }
+                }
+              }
+           };
+          this.socket = io('http://localhost:3000/chat', socketOptions);
+          this.initListeners();
+      },
+      initListeners() {
+          this.socket.on('msgToClient', (msg) => {
+              this.receiveMessage(msg);
+          });
+          this.socket.on('registered', (token) => {
+              this.useToken(token);
+          });
+          this.socket.on('UserUpdated', () => {
+              console.log('name changed');
+          });
+          this.socket.on('supplyInfo', (supply) => {
+              console.log(supply);
+          });
+          this.socket.onAny((event, ...args) => {
+              console.log(`Raised ${event}`)
+          });
+      }
+  }
+})
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
+<template> 
+  <h1>{{ title }}</h1>
+            <input v-model="namet" type="text"/>
+            <button type="submit" @click="renameUser">Rename</button>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+            <input v-model="text" type="text"/>
+            <button type="submit" @click="sendMessage">Send</button>
+        <p>
+            <ul>
+                <li v-for="msg of messages">{{ msg }}</li>
+            </ul>
+        </p>
+        <button @click="getSupply">Water</button>
+        <button @click="register">Tokenize</button>
 </template>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
