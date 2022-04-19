e2d43c2adcb04179c7cb95a0eebe14a98ec9823a
Afterflame <wither104@gmail.com>
2022-04-19 21:23:59 +1000

    menuSocket -> socket

--- client/src/stores/main.ts
+++ client/src/stores/main.ts
@@ -11,7 +11,7 @@ export const useMainStore: any = defineStore("mainStoreID", {
     isAdmin: false,
     selfId: -1,
     name: 'noname',
-    menuSocket: io('http://localhost:3000/'),
+    socket: io('http://localhost:3000/'),
   }),
   getters: {
     getToken: (state) => {
@@ -26,79 +26,78 @@ export const useMainStore: any = defineStore("mainStoreID", {
     useToken(token: string) {
       this.token = token;
       console.log('using token', token)
-      this.menuSocket.disconnect();
-      this.menuSocket = io('http://localhost:3000/', { extraHeaders: { Authorization: token } });
+      this.socket.disconnect();
+      this.socket = io('http://localhost:3000/', { extraHeaders: { Authorization: token } });
       this.initListeners();
     },
     renameUser(name: string) {
       console.log(`rename: ${name}`);
-      this.menuSocket.emit('rename', name);
+      this.socket.emit('rename', name);
     },
     //сделать async renameConfirmed как-нибудь
     register(userName: string) {
       console.log(`register: ${userName}`);
-      this.menuSocket.emit('register');
+      this.socket.emit('register');
     },
     createRoom(roomName: string) {
       console.log(`creating room: ${roomName}`);
-      this.menuSocket.emit('createRoom', roomName);
+      this.socket.emit('createRoom', roomName);
     },
     joinRoom(roomId: number) {
       console.log(`joining room: ${roomId}`);
-      this.menuSocket.emit('joinRoom', roomId);
+      this.socket.emit('joinRoom', roomId);
     },
     leaveRoom(roomId: number) {
       console.log(`leaving room: ${roomId}`);
-      this.menuSocket.emit('leaveRoom', roomId);
+      this.socket.emit('leaveRoom', roomId);
       this.isAdmin=false;
       this.activeRoomId=0;
     },
     getRooms() {
       console.log('trying to get rooms...')
-      this.menuSocket.emit('getRooms');
+      this.socket.emit('getRooms');
     },
-    destroymenuSocket() {
+    destroysocket() {
       console.log('destroying game socket');
-      this.menuSocket.disconnect();
+      this.socket.disconnect();
     },
     startGame() {
       console.log('starting game', this.activeRoomId);
-      this.menuSocket.emit('create');
+      this.socket.emit('create');
     },
     initGameListeners() {
       console.log('listening to \'gameStarted\'')
-      this.menuSocket.on('gameStarted', (game) => {
+      this.socket.on('gameStarted', (game) => {
         router.push('/game')
       });
     },
     initListeners() {
       console.log('listening to \'registered\'')
-      this.menuSocket.on('registered', (token: string, id: number) => {
+      this.socket.on('registered', (token: string, id: number) => {
         this.selfId = id;
         this.useToken(token);
       });
       console.log('listening to \'RoomCreated\'')
-      this.menuSocket.on('RoomCreated', (id: number) => {
+      this.socket.on('RoomCreated', (id: number) => {
         console.log(`room created with id: ${id}`);
-        //this.initmenuSocket();
         this.activeRoomId = id;
         this.isAdmin = true;
       });
       console.log('listening to \'gameStarted\'')
-      this.menuSocket.on('gameStarted', (game) => {
+      this.socket.on('gameStarted', (game) => {
         router.push('/game')
       });
       console.log('listening to \'UserUpdated\'')
-      this.menuSocket.on('UserUpdated', (name: string) => {
+      this.socket.on('UserUpdated', (name: string) => {
         this.name = name;
       });
       console.log('listening to \'updateRooms\'')
-      this.menuSocket.on('updateRooms', (res) => {
+      this.socket.on('updateRooms', (res) => {
         console.log(res);
         useRoomStore().updateRooms(res)
       });
       console.log('listening to \'userJoined\'')
-      this.menuSocket.on('userJoined', (id: number, username: string, lobby: number) => {
+      this.socket.on('userJoined', (id: number, username: string, lobby: number) => {
         if (id == this.selfId) {
           this.activeRoomId = lobby;
         }
