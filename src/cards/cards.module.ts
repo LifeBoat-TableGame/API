bb5027ee9d8bd1fb145d101d54c8dcae6a2fffbd
Jeskay <37577958+Jeskay@users.noreply.github.com>
2022-04-25 14:50:53 +1000

    action service

--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -5,10 +5,11 @@ import { Character } from '../models/character.entity';
 import { Navigation } from '../models/navigation.entity';
 import { Supply } from '../models/supply.entity';
 import { CardsService } from './cards.service';
+import { ActionsService } from './actions/actions.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Character, Supply, Navigation, GameSupply])],
-  providers: [CardsService],
+  providers: [CardsService, ActionsService],
   exports: [CardsService]
 })
 export class CardsModule {}
