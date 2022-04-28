e168f6beb4c986dc89543f957be51652febb6612
Afterflame <wither104@gmail.com>
2022-04-28 21:05:14 +1000

    card overlap v0.3

--- client/src/components/Card.vue
+++ client/src/components/Card.vue
@@ -1,14 +1,14 @@
 
 <template>
-<div :class="[tilts[6+props.posFromMiddle], 'hover:rotate-0', shifts[6+props.posFromMiddle], 'cardWrapper', 'hover:scale-150']">
-  <div :class="['card', 'vertical-container', 'box', 'noselect',]" @click="useCard">
+  <div
+    :class="['card', 'vertical-container', 'box', 'noselect', tilts[6 + props.posFromMiddle], shifts[6 + props.posFromMiddle]]"
+    @click="useCard">
     <div>{{ supply.name }}
     </div>
     <div>{{ supply.description }}
     </div>
-    <div>{{ supply.strength }} {{props.posFromMiddle}}
+    <div>{{ supply.strength }} {{ props.posFromMiddle }}
     </div>
-  </div>
   </div>
 </template>
 
@@ -23,34 +23,34 @@ const roomStore = useRoomStore();
 
 
 const tilts = [
-"rotate-[-12deg]",
-"rotate-[-10deg]",
-"rotate-[-8deg]",
-"rotate-[-6deg]",
-"rotate-[-4deg]",
-"rotate-[-2deg]",
-"rotate-[-0deg]",
-"rotate-[2deg]",
-"rotate-[4deg]",
-"rotate-[6deg]",
-"rotate-[8deg]",
-"rotate-[10deg]",
-"rotate-[12deg]",
+  "rotate-[-12deg]",
+  "rotate-[-10deg]",
+  "rotate-[-8deg]",
+  "rotate-[-6deg]",
+  "rotate-[-4deg]",
+  "rotate-[-2deg]",
+  "rotate-[-0deg]",
+  "rotate-[2deg]",
+  "rotate-[4deg]",
+  "rotate-[6deg]",
+  "rotate-[8deg]",
+  "rotate-[10deg]",
+  "rotate-[12deg]",
 ]
 const shifts = [
-"translate-y-[1.5rem]",
-"translate-y-[1.2rem]",
-"translate-y-[0.8rem]",
-"translate-y-[0.5rem]",
-"translate-y-[0.2rem]",
-"translate-y-[0.05rem]",
-"translate-y-[0rem]",
-"translate-y-[0.05rem]",
-"translate-y-[0.2rem]",
-"translate-y-[0.5rem]",
-"translate-y-[0.8rem]",
-"translate-y-[1.2rem]",
-"translate-y-[1.5rem]",
+  "translate-y-[1.9rem]",
+  "translate-y-[1.4rem]",
+  "translate-y-[1rem]",
+  "translate-y-[0.5rem]",
+  "translate-y-[0.25rem]",
+  "translate-y-[0.05rem]",
+  "translate-y-[0rem]",
+  "translate-y-[0.05rem]",
+  "translate-y-[0.25rem]",
+  "translate-y-[0.5rem]",
+  "translate-y-[1rem]",
+  "translate-y-[1.4rem]",
+  "translate-y-[1.9rem]",
 ]
 
 const props = defineProps<{
@@ -62,29 +62,26 @@ const props = defineProps<{
 const emit = defineEmits(['card:clicked'])
 
 
-const startIndex = 7+props.posFromMiddle
+const startIndex = 7 + props.posFromMiddle
 const useCard = () => {
   emit('card:clicked');
 }
 </script>
 
 <style scoped>
-.cardWrapper {
-  @apply overflow-hidden;
-}
-
-.cardWrapper:last-child,
-.cardWrapper:hover {
-  @apply overflow-visible
-}
 .card {
-    @apply bg-light-bg 
-    text-2xl
-    hover:text-3xl
-    border-2 border-dark-blue 
-    hover:border-deep-blue
-    w-24 h-36
-    origin-[50%_90%]
+  @apply 
+  bg-light-bg 
+  text-2xl 
+  hover:text-3xl 
+  border-2 
+  w-24 h-36
+  border-dark-blue 
+  hover:border-deep-blue 
+  origin-[50%_100%] 
+  hover:z-10
+  hover:rotate-0 
+  hover:scale-150
 }
 </style>
 
--- client/src/components/Hand.vue
+++ client/src/components/Hand.vue
@@ -1,11 +1,11 @@
-
 <template>
   <div class="hand">
       <Card v-for="(supply, index) in supplies" 
       :key="supply.name" 
       :posFromMiddle="index-Math.ceil(supplies.length/2)+1" 
       :supply=supply 
-      :tilted="true" 
+      :tilted="true"
+      v-bind:class="[index!=0 ? overlapVal : '']"
       >
       </Card>
   </div>
@@ -17,6 +17,7 @@ import { useMainStore } from '../stores/main';
 import { useRoomStore } from '../stores/rooms';
 import { Supply } from '../interfaces/game';
 import Card from './Card.vue';
+import { computed } from '@vue/reactivity';
 const title = 'ActiveRoomMenu';
 const mainStore = useMainStore();
 const roomStore = useRoomStore();
@@ -26,7 +27,9 @@ const props = defineProps<{
   supplies: Supply[];
 }>();
 const emit = defineEmits(['card:clicked'])
-
+const overlapVal = computed(() => {
+  return 'ml-[-' + (6-(30-6)/(props.supplies.length-1))+ 'rem]'
+});
 </script>
 <style scoped>
 
